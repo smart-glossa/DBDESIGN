37e86f11ca833399db689aa22f4f0fe4ef5c6d19
selvips <lashmiselvi@smartglossa.com>
2017-01-12 16:54:26 +0530

    add files

--- dbdesign/src/main/java/com/smartglossa/dbdesign/dbdesignClass.java
+++ dbdesign/src/main/java/com/smartglossa/dbdesign/dbdesignClass.java
@@ -8,19 +8,27 @@ import java.sql.Statement;
 import org.json.JSONObject;
 
 public class dbdesignClass {
-	 Connection conn = null;
-	    Statement stmt = null;
-	    ResultSet rs = null;
-	    public dbdesignClass()throws ClassNotFoundException, SQLException {
-	        openConnection();
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
 
-	    }
-		public void adddealer(int dealerId, String name, String address, String phoneNumber, String TINNumber)
-	            throws SQLException, ClassNotFoundException {
-	        JSONObject obj = new JSONObject();
+	public dbdesignClass() throws ClassNotFoundException, SQLException {
+		openConnection();
+
+	}
+
+	private void openConnection() {
+		// TODO Auto-generated method stub
 		
-		}
+	}
 
+	public void addDB(int Id, String type, int size, String defel, String keyword)
+			throws SQLException, ClassNotFoundException {
+		JSONObject obj = new JSONObject();
+
+		String query = "insert into design(id,type,size,defel,keyword) values(" + Id + ",'" + type + "'," + size + ",'"
+				+ defel + "'," + keyword + "')";
+
+	}
 
 }
-
--- dbdesign/src/main/webapp/js/deed.js
+++ dbdesign/src/main/webapp/js/deed.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
 
-	var div = document.createElement("div");
-	div.className = "add";
+$(document).on('click','#submit',function(){
+	var form="";
+	form += "";
+	form += "<table>";
+	form += "<tr>";
+	form += "<td>name<\/td>";
+	form += "<td><input type=\"text\"><\/input><\/td><\/tr>";
+	form += "<tr>";
+	form += "<td>type<\/td>";
+	form += "<td><input type=\"text\"><\/input><\/td><\/tr><tr>";
+	form += "<td>size<\/td>";
+	form += "<td><input type=\"text\"><\/input><\/td><\/tr><tr>";
+	form += "<td>default<\/td>";
+	form += "<td><input type=\"text\"><\/input><\/td><\/tr>";
+	form += "<\/table>";
+	form+="<input type=\"checkbox\">primary key<\/input><br>";
+	form+="<input type=\"checkbox\">Allow nulls<\/input><br>";
+	form+="<input type=\"checkbox\">unique<\/input><br>";
+	form+="<input type=\"checkbox\">Auto increment<\/input><br>";
+	form+="<input type=\"checkbox\">foerign key<\/input><br>";
+	form += "<input type=\"submit\" value=\"save\"><\/input><br>";
+	form += "";
+	$('.add')[0].innerHTML=form;
+	$('.add').toggle();
 	
-	$("body")[0].appendChild(div);
-	$(".add")[0].appendChild(myform());
+});
 });
