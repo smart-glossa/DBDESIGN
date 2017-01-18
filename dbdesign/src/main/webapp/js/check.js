e68b66ed202aaa589fdc547bccf2d0048142e39f
selvips <lashmiselvi@smartglossa.com>
2017-01-18 16:09:09 +0530

    model changes

--- dbdesign/src/main/webapp/js/check.js
+++ dbdesign/src/main/webapp/js/check.js
@@ -1,14 +1,15 @@
 function my(){
  
-   var name=$('#Name').val(); 
-   var type=$('#Type').val();  
-   var size=$('#Size').val();  
-   var defaults=$('#Dfult').val();  
+   var name=$('.Name').val(); 
+   var type=$('.Type').val();  
+   var size=$('.Size').val();  
+   var defaults=$('.Dfult').val(); 
+   var key=$('.key').val();
     
 	 var tble="";
 	 tble += "<table id=\"dfr\"><tr>";
-	 tble += "<th>Name<\/th><th>Type<\/th><th>size<\/th><th>defaukts<\/th><\/tr>";
-	 tble += "<tr><td><input type=\"text\" id=\"Name\" value=\""+name+"\"><\/td><td><input type=\"text\" id=\"Type\" value=\""+type+"\"><\/td><td><input type=\"text\" id=\"Size\" value=\""+size+"\"><\/td><td><input type=\"text\" id=\"Dfult\" value=\""+defaults+"\"><\/td><\/tr>";
+	 tble += "<th>Name<\/th><th>Type<\/th><th>size<\/th><th>defaukts<\/th><th>key</th><\/tr>";
+	 tble += "<tr><td><input type=\"text\" id=\"Name\" value=\""+name+"\"><\/td><td><input type=\"text\" id=\"Type\" value=\""+type+"\"><\/td><td><input type=\"text\" id=\"Size\" value=\""+size+"\"><\/td><td><input type=\"text\" id=\"Dfult\" value=\""+defaults+"\"><\/td><td><input type=\"text\" class=\"key\" value=\""+key+"\"><\/tr>";
 	 tble += "<\/table>";
 
 	 $('#get')[0].innerHTML=tble;
