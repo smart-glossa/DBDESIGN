ba8eba3f1ebe81eeaaeb407f527667310a9b6b46
selvips <lashmiselvi@smartglossa.com>
2017-01-24 10:20:08 +0530

    change the error

--- dbdesign/src/main/webapp/js/check.js
+++ dbdesign/src/main/webapp/js/check.js
@@ -1,9 +1,9 @@
 function my(){
-   var name=$('.Name').val(); 
-   var type=$('.Type').val();  
-   var size=$('.Size').val();  
-   var defaults=$('.Dfult').val(); 
-   var key=$('.key').val();
+   var name=$('#Name').val(); 
+   var type=$('#Type').val();  
+   var size=$('#Size').val();  
+   var defaults=$('#Dfult').val(); 
+   var key=$('#key').val();
     
 	 var tble="";
 	 tble += "<table id=\"dfr\"><tr>";
@@ -14,3 +14,21 @@ function my(){
 	 $('#get')[0].innerHTML=tble;
  }
  
+function edit_row(no)
+{
+	document.getElementById("edit_button"+no).style.display="none";
+	document.getElementById("save_button"+no).style.display="block";
+	
+	var name=document.getElementById("Name"+no);
+	var type=document.getElementById("Type"+no);
+	var size=document.getElementById("Size"+no);
+	
+	var name_data=name.innerHTML;
+	var type_data=type.innerHTML;
+	var size_data=size.innerHTML;
+	
+	name.innerHTML="<input type='text' id='name_text"+no+"' value='"+name_data+"'>";
+	country.innerHTML="<input type='text' id='country_text"+no+"' value='"+type_data+"'>";
+	age.innerHTML="<input type='text' id='age_text"+no+"' value='"+size_data+"'>";
+	
+}
