24405582cd679e3cefec9279434b3969cf283053
selvips <lashmiselvi@smartglossa.com>
2017-01-27 14:57:49 +0530

    change the js file

--- dbdesign/src/main/webapp/js/div.js
+++ dbdesign/src/main/webapp/js/div.js
@@ -1,42 +1,83 @@
 $(document).ready(function(){
-	$(document).on(
-			"click",
-			"#login",
-			function(key) {
-				var userName = $('#userName').val();
-				var pass = $('#pass').val();
+	if(getCookie("userName") != undefined){
+		window.location.href = "index.html";
+	}
+	$(document).on('click','#add',function(){
+		var Name = $('#Name').val();
+		var userName = $('#userName').val();
+		var pass = $('#pass').val();
+	
+		if(name===""){
+			$('#name').focus().css('outline-color','red');
+			return false;
+		}
+		if(userName===""){
+			$('#userName').focus().css('outline-color','red');
+			return false;
+		}
+		if(pass===""){
+			$('#pass').focus().css('outline-color','red');
+			return false;
+		}
+   var url="http://localhost:8080/dbdesign/User?operation=userAdd&name="+Name+"&userName="+userName+"&pass="+pass;
+		$.ajax({
+			url:url,
+			type:'POST'
+		})
+		.done(function(result){
+			var res = JSON.parse(result);
+			if (res.status == 1){
+				alert("Added Sussfully");
+			}
+			$('#Name').val("");
+			$('#userName').val("");
+			$('#pass').val("");
+			
+		})
+		.fail(function(result){
+			alert(result);
+		})
+	}).on('click','#login',function(){
+		var userName = $('#userName').val();
+		var pass = $('#passw').val();
+		if(userName===""){
+			$('#user').focus().css('outline-color','red');
+			return false;
+		}
+		if(pass===""){
+			$('#passw').focus().css('outline-color','red');
+			return false;
+		}
+		var url ="http://localhost:8080/dbdesign/User?operation=logIn&userName="+userName+"&pass="+pass; 
+		$.ajax({
+			url:url,
+			type:'POST'
+		})
+		.done(function(result){
+			var res = JSON.parse(result);
+			if(res.status == 1){
+				document.cookie = "userName="+ uname;
+				window.location.href = "index.html";
+			}
+		})
+		.fail(function(result){
+			alert(result);
+		})
+	})
+	
 
-				if (userName == "") {
-					alert("Please Enter username ");
-					$("#userName").focus().css("outline-color", "#ff0000");
-				}
-				if (pass == "") {
-					alert("Please Enter password");
-					$("#pass").focus().css("outline-color", "ff0000");
-					return;
-				}
-				var url ="http://localhost:8080/dbdesign/User?operation=logIn&userName="+userName+"&pass="+pass;
-				$.ajax({
-					url : url,
-					type : 'POST'
-				}).done(function(result) {
-					var resp = JSON.parse(result);
-					if (resp.status == "success") {
-						
-						document.cookie = "userName=" + user;
-						 window.location.href = 'index.html';
-						
-						
-						
-					} else {
-						if (resp.status == "error") {
-							alert("Incorrect username /password");
-						}
-					}
-
-				}).fail(function(result) {
-					console.log(result);
-				});
-
-			});
-	});
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return undefined;
+}
+});
