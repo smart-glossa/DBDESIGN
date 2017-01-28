18ca414a007a35afc109e69bc01d2f790b5482ae
selvips <lashmiselvi@smartglossa.com>
2017-01-28 10:12:45 +0530

    add table class and servlet

--- dbdesign/src/main/java/com/smartglossa/protable/ProtableClass.java
+++ dbdesign/src/main/java/com/smartglossa/protable/ProtableClass.java
@@ -1,9 +1,15 @@
 package com.smartglossa.protable;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.smartglossa.dbdesign.UserConstrant;
 
 public class ProtableClass {
 
@@ -16,6 +22,72 @@ public class ProtableClass {
 	}
 
  	public void proAdd(String tableName,int projectId) throws SQLException {
- 		
+ 		try {
+			String qry = "insert into protable(tableName,projectId)values('"
+					+ tableName + "'," + projectId + ")";
+			sta.execute(qry);
+		} finally {
+			closeConnection();
+		}
+
+	}
+ 	public JSONArray getall() throws SQLException, ClassNotFoundException {
+        JSONArray result = new JSONArray();
+        try {
+            String query = "select * from protable";
+            res = sta.executeQuery(query);
+            while (res.next()) {
+                JSONObject get = new JSONObject();
+                get.put("tableId", res.getInt(1));
+                get.put("tableName", res.getString(2));
+                get.put("projectId", res.getInt(3));
+               
+               
+                result.put(get);
+            }
+            return result;
+        } finally {
+            closeConnection();
+        }
+    }
+ 	public JSONObject getone(int tableId) throws SQLException, ClassNotFoundException {
+	        JSONObject one = new JSONObject();
+	        try {
+	            String query = "select * from protable where tableId="+tableId;
+	            res = sta.executeQuery(query);
+	            if (res.next()) {
+	            	 one.put("tableName", res.getString(2));
+	                 one.put("projectId", res.getString(3));
+	                
+	                  
+	            }
+
+	        } finally {
+	            closeConnection();
+	        }
+	        return one;
+
+	    }
+	  
+ 	private void openConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://" + UserConstrant.MYSQL_SERVER + "/" + UserConstrant.DATABASE_NAME,
+				UserConstrant.USERNAME, UserConstrant.PASSWORD);
+		sta = con.createStatement();
+
+	}
+
+	private void closeConnection() throws SQLException {
+		if (con != null) {
+			con.close();
+
+		}
+		if (sta != null) {
+			sta.close();
+		}
+		if (res != null) {
+			res.close();
+		}
 }
-}
+	}
+
--- dbdesign/src/main/java/com/smartglossa/protable/ProtableServlet.java
+++ dbdesign/src/main/java/com/smartglossa/protable/ProtableServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 
 public class ProtableServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,15 +19,47 @@ public class ProtableServlet extends HttpServlet {
 		doPost(request, response);
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		String operation=request.getParameter("operation");
-		if(operation.equals("protableAdd")){
-		String tableName=request.getParameter("tableName");
-		int projectId=Integer.parseInt(request.getParameter("projectId"));
-		
-	}
+		String operation = request.getParameter("operation");
+		if (operation.equals("addTable")) {
+			JSONObject obj = new JSONObject();
+			 String tableName = request.getParameter("tableName");
+			int projectId=Integer.parseInt( request.getParameter("projectId"));
+			
+			try {
+				ProtableClass Project = new ProtableClass();
+				Project.proAdd(tableName,projectId);
+				obj.put("status", 1);
+			} catch (Exception e) {
+				obj.put("status",0);
+				e.printStackTrace();
+			}
+		response.getWriter().println(obj);	
+	}else if (operation.equals("getAll")) {
+        JSONArray result = new JSONArray();
+        try {
+        	ProtableClass get = new ProtableClass();
+            result = get.getall();
+        } catch (Exception e) {
+            JSONObject get = new JSONObject();
+            get.put("status", 0);
+            result.put(get);
+            e.printStackTrace();
+        }
+        response.getWriter().println(result);
 
+}else if (operation.equals("getOne")) {
+    int tableId = Integer.parseInt(request.getParameter("tableId"));
+    JSONObject one = new JSONObject();
+    try {
+    	ProtableClass get = new ProtableClass();
+        one = get.getone(tableId);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    response.getWriter().println(one);
 }
+	}
 }
